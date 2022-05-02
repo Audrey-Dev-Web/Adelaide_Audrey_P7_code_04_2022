0230200fe8aa60639059bb9d7d544168649e926a
Audrey Adélaïde <audrey.adelaide.pro@gmail.com>
2022-05-02 16:35:27 +0200

    Ajout de sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bonjour !</h1>
       </header>
     </div>
   );
